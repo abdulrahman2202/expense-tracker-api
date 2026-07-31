2ccef30dc4dd9bafc805a0d9e1621359dc381d3d
abdul@gmail.com <abdulrahman2.ar44@gmail.com>
2026-07-31 12:27:49 +0530

    models set for insuring the right data will pass

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Smart Expense Tracker API",
+    description="REST API for managing personal expenses",
+    version="1.0.0"
+)
 
 @app.get("/")
 def home():
-    return{"message": "Welcome to the Expense Tracker API"}
+    return{"message": "Welcome to the Smart Expense Tracker API"}
---
+++ src/models/expense_model.py
@@ -0,0 +1,15 @@
+from datetime import date
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class ExpenseCreate(BaseModel):
+    title: str = Field(..., min_length=1, max_length=100)
+    amount: float = Field(..., gt=0)
+    category: str = Field(..., min_length=1, max_length=50)
+    date: date
+
+
+class ExpenseResponse(ExpenseCreate):
+    id: UUID
